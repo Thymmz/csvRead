30e5a0e0c64160bf530a59d237cbe186844fcbdf
Thymmz <atharva.deshpande228@gmail.com>
2022-06-09 11:25:43 -0500

    code cleanup

--- src/main/java/com/walmart/filequeuedatabase/model/People.java
+++ src/main/java/com/walmart/filequeuedatabase/model/People.java
@@ -1,6 +1,5 @@
 package com.walmart.filequeuedatabase.model;
 
-import com.opencsv.bean.CsvBindByPosition;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,39 +13,30 @@ import java.io.Serializable;
 @NoArgsConstructor
 @CsvRecord(separator = ",", skipFirstLine = true, generateHeaderColumns = true)
 public class People implements Serializable {
-    //@CsvBindByPosition(position = 0)
     @DataField(pos = 1, columnName = "person_ID")
     private String personid;
 
-    //@CsvBindByPosition(position = 1)
     @DataField(pos = 2, columnName = "name")
     private String name;
 
-    //@CsvBindByPosition(position = 2)
     @DataField(pos = 3, columnName = "first")
     private String first;
 
-    //@CsvBindByPosition(position = 3)
     @DataField(pos = 4, columnName = "last")
     private String last;
 
-    //@CsvBindByPosition(position = 4)
     @DataField(pos = 5, columnName = "middle")
     private String middle;
 
-    //@CsvBindByPosition(position = 5)
     @DataField(pos = 6, columnName = "email")
     public String email;
 
-    //@CsvBindByPosition(position = 6)
     @DataField(pos = 7, columnName = "phone")
     public String phone;
 
-    //@CsvBindByPosition(position = 7)
     @DataField(pos = 8, columnName = "fax")
     public String fax;
 
-    //@CsvBindByPosition(position = 8)
     @DataField(pos = 9, columnName = "title")
     public String title;
 }
--- src/main/java/com/walmart/filequeuedatabase/service/route.java
+++ src/main/java/com/walmart/filequeuedatabase/service/route.java
@@ -1,17 +1,14 @@
 package com.walmart.filequeuedatabase.service;
 
 import com.walmart.filequeuedatabase.model.People;
-import org.apache.camel.Endpoint;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.spi.DataFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 
 @Component
 public class route extends RouteBuilder{
@@ -35,12 +32,4 @@ public class route extends RouteBuilder{
                 .to("mq:queue:INPUT");
     }
 
-//    private void addIncomingFixMessageRoute() throws IOException {
-//        from(Endpoint.class)
-//                .routeId("IncomingFixMessageRoute")
-//                .bean(csvReader.readCsvFile(), "transform")
-//                .split(body())
-//                .marshal().json()
-//                .to("mq:queue:INPUT");
-//    }
 }
