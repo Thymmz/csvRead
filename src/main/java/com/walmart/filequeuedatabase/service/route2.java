05a1ba6b3745dd82a96a168000dfd1052e5a5c22
Thymmz <atharva.deshpande228@gmail.com>
2022-06-20 10:25:35 -0500

    route 2 done

--- src/main/java/com/walmart/filequeuedatabase/service/route2.java
+++ src/main/java/com/walmart/filequeuedatabase/service/route2.java
@@ -11,27 +11,27 @@ import org.springframework.stereotype.Component;
 public class route2 extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-//        from("mq:queue:INPUT")
-//                .unmarshal().json(JsonLibrary.Jackson, PeopleModel.class)
-//                .process(new Processor() {
-//                    public void process(Exchange xchg) throws Exception {
-//                        //Take the Employee object from the exchange and create the insert query
-//                        PeopleModel person = xchg.getIn().getBody(PeopleModel.class);
-//                        String query = "INSERT INTO people(personid,name, first, last, middle, email, phone, fax, title)values" +
-//                                "('" + person.getPersonid() + "','"
-//                                + person.getName() + "','"
-//                                + person.getFirst() +"','"
-//                                + person.getLast() +"','"
-//                                + person.getMiddle() +"','"
-//                                + person.getEmail() +"','"
-//                                + person.getPhone() +"','"
-//                                + person.getFax() +"','"
-//                                + person.getTitle() +"')";
-//                        // Set the insert query in body and call camel jdbc
-//                        xchg.getIn().setBody(query);
-//                    }
-//                })
-//                .to("jdbc:datasource");
+        from("mq:queue:INPUT")
+                .unmarshal().json(JsonLibrary.Jackson, PeopleModel.class)
+                .process(new Processor() {
+                    public void process(Exchange xchg) throws Exception {
+                        //Take the Employee object from the exchange and create the insert query
+                        PeopleModel person = xchg.getIn().getBody(PeopleModel.class);
+                        String query = "INSERT INTO people(personid,name, first, last, middle, email, phone, fax, title)values" +
+                                "('" + person.getPersonid() + "','"
+                                + person.getName() + "','"
+                                + person.getFirst() +"','"
+                                + person.getLast() +"','"
+                                + person.getMiddle() +"','"
+                                + person.getEmail() +"','"
+                                + person.getPhone() +"','"
+                                + person.getFax() +"','"
+                                + person.getTitle() +"')";
+                        // Set the insert query in body and call camel jdbc
+                        xchg.getIn().setBody(query);
+                    }
+                })
+                .to("jdbc:datasource");
                 //.log("${body}");
     }
 }
